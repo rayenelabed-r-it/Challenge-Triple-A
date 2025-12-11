b49a6ddba1746a23fe503c8706e5784e532ce142
Mohamed Mahamoud <mohamed.mahamoud0112@gmail.com>
2025-12-11 16:25:40 +0100

    changement du code pour trouver l'ip
    Replaced method of obtaining IP address for Windows compatibility.

--- monitoring_fixed_script.py
+++ monitoring_fixed_script.py
@@ -76,10 +76,7 @@ def get_system_info():
     
     # Adresse IP
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        ip_address = s.getsockname()[0]
-        s.close()
+        ip_address = socket.gethostbyname(hostname)
     except Exception:
         ip_address = "Non disponible"
     
